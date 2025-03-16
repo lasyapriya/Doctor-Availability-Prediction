4d0eaf340602ed7484fa976ac0c6180d89f0aa01
lasyapriya <csedm9@gmail.com>
2025-03-16 12:55:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ df["Speciality"] = label_encoder.transform(df["Speciality"])
 # ✅ Home route to prevent 404 errors
 @app.route("/")
 def serve_html():
-    return send_from_directory(".", "index.html")  # Serving from the current directory
+    return send_file(".", "index.html")  # Serving from the current directory
 
 @app.route("/predict", methods=["GET", "POST"])
 def predict():
